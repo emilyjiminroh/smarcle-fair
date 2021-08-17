1bbdd1734f192b28e975039c820f04bf368b8f42
Yonghyun <yh47529722@gmail.com>
2021-08-17 11:30:14 +0900

    almost done loginpage

--- javascript/login.js
+++ javascript/login.js
@@ -11,11 +11,18 @@ const CLASS_HIDDEN = "hidden";
 const NAME_KEY = "name";
 const POSITION_KEY = "position";
 
+// 이름 포지션 받으면 1 증가 -> 2 되면 버튼 생성
+let num = 0; 
+
 function nameSubmit(event) {
   event.preventDefault();
   loginNameForm.classList.add(CLASS_HIDDEN);
   const name =loginNameInput.value;
   localStorage.setItem(NAME_KEY, name);
+  num += 1;
+  if (num == 2){
+    enterBtn.classList.remove(CLASS_HIDDEN);
+  }
   paintName();
 }
 
@@ -24,36 +31,46 @@ function positionSubmit(event) {
   loginPositionForm.classList.add(CLASS_HIDDEN);
   const position =loginPositionInput.value;
   localStorage.setItem(POSITION_KEY, position);
+  num += 1;
+  if (num == 2){
+    enterBtn.classList.remove(CLASS_HIDDEN);
+  }
   paintPosition();
 }
 
 function paintName() {
   const name = localStorage.getItem(NAME_KEY);
-  loginName.innerText = `Hello ${name}`;
+  loginName.innerText = `어서오세요 ${name}님!`;
   loginName.classList.remove(CLASS_HIDDEN);
 }
 
 function paintPosition() {
   const position = localStorage.getItem(POSITION_KEY);
-  loginPosition.innerText = `Hello ${position}`;
+  loginPosition.innerText = `${position}`;
   loginPosition.classList.remove(CLASS_HIDDEN);
 }
 
 const savedName = localStorage.getItem(NAME_KEY);
 const savedPosition = localStorage.getItem(POSITION_KEY);
 
-if (savedName === null) {
-  loginNameForm.classList.remove(CLASS_HIDDEN);
-  loginNameForm.addEventListener("submit", nameSubmit);
-} else {
-  paintName();
-  paintPosition();
-}
+const enterBtn = document.querySelector(".enter");
 
-if (savedPosition === null) {
-  loginPositionForm.classList.remove(CLASS_HIDDEN);
+if (savedName === null || savedPosition === null) {
+  loginNameInput.classList.remove(CLASS_HIDDEN);
+  loginNameForm.addEventListener("submit", nameSubmit);
+  loginPositionInput.classList.remove(CLASS_HIDDEN);
   loginPositionForm.addEventListener("submit", positionSubmit);
 } else {
   paintName();
   paintPosition();
+  enterBtn.classList.remove(CLASS_HIDDEN);
 }
+
+// if (savedPosition === null) {
+  
+// } else {
+//   paintName();
+//   paintPosition();
+// }
+
+// 로컬스토리지에 값들 받으면 버튼 뜨게 하는 기능
