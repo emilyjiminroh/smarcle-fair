80fcf7952fc4701e7abadbe1c68ddf1406e7fe09
Yonghyun <yh47529722@gmail.com>
2021-08-19 20:10:26 +0900

    make hover-modal and hide name-tag

--- javascript/hover-modal.js
+++ javascript/hover-modal.js
@@ -1,14 +1,14 @@
 const hoverModal = document.querySelectorAll(".hover-modal");
 const btnsDetail = document.querySelectorAll(".button_detail");
 
-// btnsDetail.forEach((btn) => {
-//   btn.addEventListener("mouseover", () => {
-//     btn.classList.remove("hidden");
-//   });
-// })
+for (let i = 0; i < 5; i++) {
+  btnsDetail[i].addEventListener("mouseover", () => {
+    hoverModal[i].classList.remove("hidden");
+  })
+}
 
-// btnsDetail.forEach((btn) => {
-//   btn.addEventListener("mouseout", () => {
-//     btn.classList.add("hidden");
-//   });
-// })
+for (let i = 0; i < 5; i++) {
+  btnsDetail[i].addEventListener("mouseout", () => {
+    hoverModal[i].classList.add("hidden");
+  })
+}
