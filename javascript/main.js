a34979eb67f72a453efcdd8ce07db353189d2a89
emilyjiminroh <emilyjiminroh@gmail.com>
2021-08-22 19:13:37 +0900

    update main js

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,9 @@
 const nameText = document.querySelector(".name-tag__body__name");
 const visitorText = document.querySelector(".name-tag__body__visit");
 
+const nameAni = document.querySelector(".name-tag");
+const mainAni = document.querySelector(".main");
+const bgAni = document.querySelector(".bg-img-box");
+
 nameText.innerText = localStorage.getItem("name");
 visitorText.innerText = localStorage.getItem("position");
